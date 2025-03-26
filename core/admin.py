c85d16056fb24a41b76f63fd9e9f71ffbcf400c3
Vinifa21 <viniciusfa21@gmail.com>
2025-03-26 14:36:26 -0300

    Implementacao dos Models +  suas implementações no Admin  + listagem no display do admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Marca, Veiculo, Pessoa
+from .models import Marca, Veiculo, Pessoa,\
+    Parametro, MovRotativo, Mensalista, MovMensalista
 #importo models para serem usados no admin
+
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ("checkin", "checkout", "veiculo", "valor_hora", "pago", "total")
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ("mensalista", "data_pgto")
+
+
 
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
+admin.site.register(Parametro)
+admin.site.register(MovRotativo, MovRotativoAdmin)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+import math
+
 from django.db import models
 
 #Models definem a estrutura dos dados armazenados, classes, campos e como eles se comportam
@@ -29,3 +31,46 @@ class Veiculo(models.Model):
 
     def __str__(self): # permite que eu escolha a string que aparece quando chamo a classe na tela
         return  self.proprietario.nome + " | " +  self.placa
+
+
+
+class Parametro(models.Model):
+    periodo = models.CharField(max_length=29)
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2)
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.periodo
+
+
+class MovRotativo(models.Model):
+    checkin = models.DateTimeField(auto_now=False)
+    checkout = models.DateTimeField(auto_now=False, null=True, blank=True)
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2)#mudar para depois puxar sozinho dos parametros
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    pago = models.BooleanField(default=False)
+
+    def horas_total(self):
+        return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
+    def total(self):
+        if self.checkout is None:
+            return "A definir"
+        return self.valor_hora * self.horas_total()
+    def __str__(self):
+        return self.veiculo.placa
+
+class Mensalista(models.Model):
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    inicio = models.DateField()
+    valor_mes = models.DecimalField(max_digits= 8, decimal_places=2)
+
+    def __str__(self):
+        return self.veiculo.proprietario.nome + " | "+self.veiculo.placa
+
+class MovMensalista(models.Model):
+    mensalista = models.ForeignKey(Mensalista, on_delete=models.CASCADE)
+    data_pgto = models.DateField()
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.mensalista) + "  | R$" + str(self.total)
