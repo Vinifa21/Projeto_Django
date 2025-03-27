549c03212b8485b3f634e18c5d1ed2a65af9cc88
Vinifa21 <viniciusfa21@gmail.com>
2025-03-27 18:54:17 -0300

    criando READ's

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-from .views import home
+from .views import home, lista_pessoas, lista_veiculos, lista_MovRotativos, lista_mensalistas, lista_MovMensalistas
 # importo do core/views.py as views de cada url
 
 #arquivo que controla as urls do meu app /core
 urlpatterns = [
     path('', home, name='core_home'),  # quando estiver na url ...core/ eu envio a view home para o usuário
-
+    path('pessoas', lista_pessoas, name='core_lista_pesosoas'),
+    path('veiculos', lista_veiculos, name='core_lista_veiculos'),
+    path('movrot', lista_MovRotativos, name='core_lista_movrotativos'),
+    path('mensalistas', lista_mensalistas, name='core_lista_mensalistas'),
+    path('movmens', lista_MovMensalistas, name='core_lista_movmensalistas'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa, Veiculo, MovRotativo, Mensalista, MovMensalista  #importo dados de um model especifico e posso injetalo em um template
 
 #view: o que é entregue ao usuário ao ele entrar numa determinada url
 #pode ser desde uma string simples com httpResponse, ou um template em html usando render
@@ -8,8 +9,34 @@ from django.http import HttpResponse
 # com o mesmo nome do app, então colocar os templates nessa pasta
 
 def home(request):
-    context = {'mensagem': 'Ola, mundo!!!'}
-    return render(request, 'core/index.html', context)
+    context = {'mensagem': 'Ola, mundo!!!'} # variavel que sera injetada no template
+    return render(request, 'core/index.html', context)  #tenho q add o index.html
+    # na pasta templates/core desse app
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all  #injeto a variavel pessoas (contem todos os objetos de pessoas) no template
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas}) # passo para dentro do template com o nome 'pessoas', a variavel pessoa
+
+
+def lista_veiculos(request):
+    todos_veiculos = Veiculo.objects.all
+    return render(request, 'core/lista_veiculos.html', {'todos_veiculos': todos_veiculos})
+
+def lista_MovRotativos(request):
+    MovsRotativos = MovRotativo.objects.all
+    return render(request, 'core/lista_MovsRotativos.html', {'variavel_injetada': MovsRotativos})
+
+
+def lista_mensalistas(request):
+    mensalistas = Mensalista.objects.all
+    return render(request, 'core/lista_mensalista.html', {'mensalistas': mensalistas})
+
+
+def lista_MovMensalistas(request):
+    movsmensalistas = MovMensalista.objects.all
+    return render(request, 'core/lista_movsmensalistas.html', {'movs': movsmensalistas})
+
+
 
 def teste(request):
     context = {'mensagem': 'Ola, mundo!!!'} # esse puxa um template direto da pasta raiz
